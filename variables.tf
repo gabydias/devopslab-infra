bb6904ad0cb547af8e1b8a8367a1ff234dca114f
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2024-03-09 19:06:01 -0300

    Update Files

--- variables.tf
+++ variables.tf
@@ -6,11 +6,5 @@ variable "project_id" {
 variable "region" {
   description = "Google Cloud region"
   type        = string
-  default     = "us-west1"
-}
-
-variable "zone" {
-  description = "Google Cloud zone"
-  type        = string
-  default     = "us-west1-b"
+  default     = "us-east1"
 }
